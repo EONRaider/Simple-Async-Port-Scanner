8049914c1adee788c28c0094b82a43a9b8efe108
EONRaider <livewire_voodoo@protonmail.com>
2021-01-29 12:37:34 -0300

    Modify CLI options parser
    - Added example usages as epilog to help prompt
    - Implemented parse_ports function
    - Modifications to options help prompt

--- simple_async_scan.py
+++ simple_async_scan.py
@@ -39,22 +39,44 @@ async def scanner(target_addresses: Iterable, ports: Iterable) -> NoReturn:
 if __name__ == '__main__':
     import argparse
 
-    usage_example = ('Usage example:\n'
-                     'python3 simple_async_scan.py '
-                     '45.33.32.156 65.61.137.117 18.192.172.30'
-                     '-p 21 22 25 80 111 135 139 443 3306 5900')
+
+    def parse_ports(ports) -> Iterator[int]:
+        """
+        Yields an iterator with integers extracted from a string
+        consisting of mixed port numbers and/or ranged intervals.
+        Ex: From '20-25,53,80,111' to (21,22,25,26,27,28,29,30,53,80)
+        """
+        for port in ports.split(','):
+            try:
+                yield int(port)
+            except ValueError:
+                start, end = (int(port) for port in port.split('-'))
+                yield from range(start, end + 1)
+
+
+    usage = ('Usage examples:\n'
+             '1. python3 simple_async_scan.py google.com -p 80,443\n'
+             '2. python3 simple_async_scan.py '
+             '45.33.32.156,demo.testfire.net,18.192.172.30 '
+             '-p 20-25,53,80,111,135,139,443,3306,5900')
 
     parser = argparse.ArgumentParser(
         description='Simple asynchronous TCP Connect port scanner',
-        epilog=usage_example,
+        epilog=usage,
         formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('targets', nargs='+', type=str, metavar='IP_ADDRESSES',
-                        help="A space-separated list of IP addresses to scan, "
-                             "e.g., '45.33.32.156 65.61.137.117'.")
-    parser.add_argument('-p', '--ports', nargs='+', type=int, required=True,
-                        help="A space-separated list of ports to scan on each "
-                             "specified target, e.g., '21 22 80 443'.")
+    parser.add_argument('targets', type=str, metavar='IP_ADDRESSES',
+                        help="A comma-separated sequence of IP addresses "
+                             "and/or domain names to scan, e.g., "
+                             "'45.33.32.156,65.61.137.117,"
+                             "testphp.vulnweb.com'.")
+    parser.add_argument('-p', '--ports', type=str, required=True,
+                        help="A comma-separated sequence of port numbers "
+                             "and/or port ranges to scan on each target "
+                             "specified, e.g., '20-25,53,80,443'.")
     args = parser.parse_args()
 
-    asyncio.run(scanner(**vars(args)))
+    target_sequence: list = args.targets.split(',')
+    port_sequence = parse_ports(args.ports)
+
+    asyncio.run(scanner(target_addresses=target_sequence, ports=port_sequence))
